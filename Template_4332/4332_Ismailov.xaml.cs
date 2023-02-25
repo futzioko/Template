7c2019f5e8b633a30bf2306de5d6a1706bbe436d
JobaDrabitel <106743609+JobaDrabitel@users.noreply.github.com>
2023-02-25 22:36:11 +0300

    Im(Ex)port
    Сделал вроде как +- рабочий код, но подключить библиотеку для работы с экселем не могу, так что оно не будет работать. сортировку по должности сделаю когда получится подключить библиотеку ClosedXML

--- Template_4332/4332_Ismailov.xaml.cs
+++ Template_4332/4332_Ismailov.xaml.cs
@@ -23,5 +23,62 @@ namespace Template_4332
         {
             InitializeComponent();
         }
+
+        private void Import_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var workers = XlsxHelper.EnumerateMetrics("Data.xlsx").ToList();
+            workersDataGrid.ItemsSource = workers;
+        }
+
+        private void Export_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Excel.Application excel = new Excel.Application();
+            excel.Visible = true;
+            Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
+            Worksheet sheet1 = (Worksheet)workbook.Sheets[1];
+
+            for (int j = 0; j < workersDataGrid.Columns.Count; j++)
+            {
+                Range myRange = (Range)sheet1.Cells[1, j + 1];
+                sheet1.Cells[1, j + 1].Font.Bold = true;
+                sheet1.Columns[j + 1].ColumnWidth = 15;
+                myRange.Value2 = workersDataGrid.Columns[j].Header;
+            }
+            for (int i = 0; i < workersDataGrid.Columns.Count; i++)
+            {
+                for (int j = 0; j < workersDataGrid.Items.Count; j++)
+                {
+                    TextBlock b = workersDataGrid.Columns[i].GetCellContent(workersDataGrid.Items[j]) as TextBlock;
+                    Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[j + 2, i + 1];
+                    myRange.Value2 = b.Text;
+                }
+            }
+        }
+        static IEnumerable<Worker> EnumerateMetrics(string xlsxpath)
+        {
+            // Открываем книгу
+            using (var workbook = new XLWorkbook(xlsxpath))
+            // Берем в ней первый лист
+            using (var worksheet = workbook.Worksheets.Worksheet(1))
+            {
+                // Перебираем диапазон нужных строк
+                for (int row = 2; row <= 11; ++row)
+                {
+                    // По каждой строке формируем объект
+                    var worker = new Worker
+                    {
+                        Id = worksheet.Cell(row, 1).GetValue<string>(),
+                        Fucntion = worksheet.Cell(row, 2).GetValue<string>(),
+                        FIO = worksheet.Cell(row, 3).GetValue<string>(),
+                        Login = worksheet.Cell(row, 4).GetValue<string>(),
+                        Password = worksheet.Cell(row, 5).GetValue<string>(),
+                        LatestEntry = worksheet.Cell(row, 6).GetValue<string>(),
+                        TypeOfEntry = worksheet.Cell(row, 4).GetValue<string>(),
+                    };
+                    // И возвращаем его
+                    yield return worker;
+                }
+            }
+        }
     }
 }
---
+++ Template_4332/Worker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_4332
+{
+     public class Worker
+    {
+        public string Id { get; set; }
+        public string Fucntion { get; set; }
+        public string FIO { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string LatestEntry   { get; set; }
+        public string TypeOfEntry { get; set; }
+    }
+}
