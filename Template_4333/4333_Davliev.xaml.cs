01b4df5c8f5d3f63329bb01cb9951610bebba5c0
Almaz <52231348+Heahdix@users.noreply.github.com>
2023-02-25 12:59:11 +0300

    Изменил кнопку
    Назвал ее своим именем

--- Template_4333/4333_Davliev.xaml.cs
+++ Template_4333/4333_Davliev.xaml.cs
@@ -23,5 +23,10 @@ namespace Template_4333
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
