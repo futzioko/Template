c54af28aa4c78de3095e0fc72c33c6246e06cfaf
Mikhail <misha.safronov.02.12@mail.ru>
2023-03-03 21:28:30 +0300

    1

--- Template_4332/4332_SafronovWindow.xaml.cs
+++ Template_4332/4332_SafronovWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Template_4332
 {
@@ -22,6 +24,51 @@ namespace Template_4332
         public _4332_SafronovWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnImport_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog()
+            {
+                DefaultExt = "*.xls;*.xlsx",
+                Filter = "файл Excel (Spisok.xlsx)|*.xlsx",
+                Title = "Выберите файл базы данных"
+            };
+            if (!(ofd.ShowDialog() == true))
+                return;
+
+            string[,] list;
+            Excel.Application ObjWorkExcel = new Excel.Application();
+            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(ofd.FileName);
+            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
+            var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
+            int _columns = (int)lastCell.Column;
+            int _rows = (int)lastCell.Row;
+            list = new string[_rows, _columns];
+            for (int j = 0; j < _columns; j++)
+               for (int i = 1; i < _rows; i++)
+                    list[i, j] = ObjWorkSheet.Cells[i + 1, j + 1].Text;
+            ObjWorkBook.Close(false, Type.Missing, Type.Missing);
+            ObjWorkExcel.Quit();
+            GC.Collect();
+
+            using (isrpo_l1Entities2 db = new isrpo_l1Entities2())
+            {
+                t row = new t();
+                for (int i = 0; i < _rows; i++)
+                {
+                    row = new t(Convert.ToInt32(list[i, 0].Substring(0, 3)), list[i, 1], list[i, 2], list[i, 3], list[i, 4], list[i, 5], list[i, 6]);
+                    db.t.Add(row);
+                }
+
+                db.SaveChanges();
+            }
+
         }
     }
 }
