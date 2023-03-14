4f1b60a4dc9abb6a726cd5c9c8c47015012d6d61
Adel <email@gmail.com>
2023-03-14 12:32:43 +0300

    Добавил на главное окно кнопку со своей фамилией, создал новое окно, добавил обработчик событий при нажатии на кнопку.

---
+++ Template_4335/Windows/Nurmiev_4335.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Template_4335.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для Nurmiev_4335.xaml
+    /// </summary>
+    public partial class Nurmiev_4335 : Page
+    {
+        public Nurmiev_4335()
+        {
+            InitializeComponent();
+        }
+    }
+}
